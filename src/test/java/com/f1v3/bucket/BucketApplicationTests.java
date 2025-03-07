26a053651277a387a1e50794775a707d90c0797c
seungjo <f1v3@kakao.com>
2025-03-07 14:51:19 +0900

    test: bucket test

--- src/test/java/com/f1v3/bucket/BucketApplicationTests.java
+++ src/test/java/com/f1v3/bucket/BucketApplicationTests.java
@@ -1,5 +1,13 @@
 package com.f1v3.bucket;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import io.github.bucket4j.Bandwidth;
+import io.github.bucket4j.Bucket;
+import io.github.bucket4j.Refill;
+import java.time.Duration;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -10,4 +18,19 @@ class BucketApplicationTests {
     void contextLoads() {
     }
 
+    @Test
+    @DisplayName("Basic Usage Test")
+    void test1() {
+        Refill refill = Refill.intervally(10, Duration.ofMinutes(1));
+        Bandwidth limit = Bandwidth.classic(10, refill);
+        Bucket bucket = Bucket.builder()
+                              .addLimit(limit)
+                              .build();
+
+        for (int i = 1; i <= 10; i++) {
+            assertTrue(bucket.tryConsume(1));
+        }
+
+        assertFalse(bucket.tryConsume(1));
+    }
 }
