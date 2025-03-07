b0d30ccc8eb7880cdbbf98de06ec8ef8783c2b24
seungjo <f1v3@kakao.com>
2025-03-07 14:13:30 +0900

    first commit

---
+++ src/main/java/com/f1v3/bucket/BucketApplication.java
@@ -0,0 +1,13 @@
+package com.f1v3.bucket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BucketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BucketApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/f1v3/bucket/BucketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.f1v3.bucket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BucketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
