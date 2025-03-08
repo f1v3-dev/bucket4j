6caf1ed313fcec4e758d44b2444de853d6e8b045
seungjo <f1v3@kakao.com>
2025-03-08 17:37:00 +0900

    feat: rate limit 환경변수 클래스 추가

--- src/main/java/com/f1v3/bucket/BucketApplication.java
+++ src/main/java/com/f1v3/bucket/BucketApplication.java
@@ -2,8 +2,10 @@ package com.f1v3.bucket;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class BucketApplication {
 
     public static void main(String[] args) {
